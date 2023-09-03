58616b8d4fee55cbe91d1b33be8778e53950f8c2
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-09-03 16:15:05 +0300

    done

--- lib/debug.py
+++ lib/debug.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy_sandbox import Student
 
 engine = create_engine('sqlite:///students.db')
+Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
